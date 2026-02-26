48b8fb7fe503a393ef70df7d365bdaafe7d20fcf
Kritika Gautam <118198758+kritika1818@users.noreply.github.com>
2026-02-26 10:54:55 +0545

    Refactor login setup to use variables for credentials

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,9 +4,10 @@ import path from "path";
 
 setup("Verify login functionality user", async ({ page }) => {
   await page.goto("https://alpha-3.ifrc-go.dev.togglecorp.com/login");
-
- await page.locator('input[type="text"], input[name*="email"]').fill(process.env.Go_Username!);
-await page.locator('input[type="password"]').fill(process.env.Go_Password!);
+  const username = process.env.Go_Username;
+  const password = process.env.Go_Password;
+   await page.locator('input[type="text"], input[name*="email"]').fill(username);
+  await page.locator('input[type="password"]').fill(password);
   await page.getByRole("button", { name: "Login" }).click();
 
   await expect(page.getByRole("heading", { level: 1, name: "IFRC Disaster Response and Preparedness" })).toBeVisible();
